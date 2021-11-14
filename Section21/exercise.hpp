1ba228b4c3af6a60c190842f90ddc04370849199
Kell Skog <kell.skog@gmail.com>
2021-11-14 14:15:11 +0100

    Begin Section 21

---
+++ Section21/exercise.hpp
@@ -0,0 +1,4 @@
+#ifndef SECTION21_EXERCISE
+#define SECTION21_EXERCISE
+
+#endif // Include guard
---
+++ Section21/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "exercise.hpp"
+
+void DeclaringAndUsingLambdaFunctions_216()
+{
+}
+
+int main()
+{
+    std::cout << "Section 21: Lambda Functions\n";
+    DeclaringAndUsingLambdaFunctions_216();
+}
